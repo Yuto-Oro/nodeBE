2e7df37c611d6929042277d2827019cd7b4bae56
Omar Orlando <monkeydoro@gmail.com>
2022-05-12 21:36:50 -0500

    Adding initial files for nodeBE API

---
+++ network/response.js
@@ -0,0 +1,13 @@
+exports.success = function (req, res, msg, status) {
+    res.status(status || 200).send({
+        error: '',
+        body: msg
+    });
+}
+
+exports.error = function (req, res, msg, status) {
+    res.status(status || 500).send({
+        error: msg,
+        body: ''
+    });
+}
